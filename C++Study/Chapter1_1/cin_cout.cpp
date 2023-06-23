4d5b92c52c56957f506840bdd879691611113577
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-24 00:20:00 +0900

    ➕[Feat] 입출력 스트림과의 첫 만남 cin, cout

---
+++ C++Study/Chapter1_1/cin_cout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>		// cout, cin, endl
+#include <cstdio>		// printf
+
+int main() {
+	using namespace std;
+	// 입력
+	int x;
+	cin >> x;
+
+	// 출력
+	cout << "I love C++" << endl;
+	cout << "I am" << x << "years old!" << endl;
+
+	return 0;
+}
