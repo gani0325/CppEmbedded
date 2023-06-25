f96f23b391f38996b7fdb28b4c2c77c309a01903
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-25 23:12:43 +0900

    ➕[Feat] 선언과 정의의 분리

---
+++ C++Study/Chapter1_1/Declaration.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b);			// forward declaration
+int subtract(int a, int b);		// forward declaration
+
+int main()
+{
+	cout << add(3, 4) << endl;
+	cout << subtract(2, 3) << endl;
+	return 0;
+}
+
+// definition
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int subtract(int a, int b)
+{
+	return a - b;
+}
