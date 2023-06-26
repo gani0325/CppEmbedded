c96d793ac2fc9d78d50dd682377163d21cedf3f2
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-26 23:59:26 +0900

    ➕[Feat] 프로젝트 생성과 출력

---
+++ C++MakeGames/BasicGrammer/main.cpp
@@ -0,0 +1,12 @@
+// #include : 헤더 파일을 여기에 포함시키는 기능
+#include <iostream>
+
+// 실행 과정 : 컴파일(번역 작업) -> 빌드 -> 실행
+
+// 시작점은 main 함수
+int main() {
+	// std 라는 namespace -> 이름 겹치는거 방지
+	// cout : 콘솔창에 출력
+	std::cout << "hi";
+	return 0;
+}
