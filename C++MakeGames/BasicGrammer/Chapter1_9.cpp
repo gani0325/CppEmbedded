b360edc167cb6ff0ab757965fbdf74d3b0660d61
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-29 00:22:49 +0900

    ➕[Feat] 삼각형으로 별 찍기

--- C++MakeGames/BasicGrammer/Chapter1_9.cpp
+++ C++MakeGames/BasicGrammer/Chapter1_9.cpp
@@ -21,6 +21,25 @@ int main()
 			cout << "*";
 		}
 		cout << endl;
-	}	
+	}
+
+	// 삼각형 별 찍기
+	// 공백		별
+		//3		1
+		//2		3
+		//1		5
+		//0		7
+	for (int i = 0; i < 4; i++) {
+		// 공백을 위한 문
+		for (int j = 0; j < 3 - i; j++) {
+			cout << " ";
+		}
+		// *을 위한 문
+		for (int j = 0; j < i * 2 + 1; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
 	return 0;
 }
