bd68002a050a16cce9720fc3bee32d723523e6ba
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-29 00:39:12 +0900

    ➕[Feat] 다이아몬드로 별 찍기

--- C++MakeGames/BasicGrammer/Chapter1_9.cpp
+++ C++MakeGames/BasicGrammer/Chapter1_9.cpp
@@ -5,12 +5,33 @@ using namespace std;
 
 int main()
 {
-	// 2단부터 9단까지 구구단
-	for (int i = 2; i < 10; i++) {
-		cout << i << "구구단" << endl;
-		for (int j = 1; j < 10; j++) {
-			cout << i << " * " << j << " = " << (i * j) << endl;
+	// 다이아몬드로 별 찍기
+	int iLine = 7;
+	int iCnt = 0;
+
+	for (int i = 0; i < iLine; i++) {
+		// 공백 : 3 2 1 0 1 2 3
+		// 별 : 1 3 5 7 5 3 1
+		iCnt = i;
+
+		// i값이 4, 5, 6 일때만 들어감
+		if (i > iLine / 2) {
+			iCnt = iLine - 1 - i;
 		}
-	}	
+
+		// i값이 0, 1, 2, 3 일때는 그대로 iCnt 대입
+		// i값이 4, 5, 6 일때는 iCnt 2, 1, 0 대입
+		// 즉, i는 0, 1, 2, 3, 2, 1, 0
+
+		for (int j = 0; j < 3 - iCnt; j++) {
+			cout << " ";
+		}
+
+		for (int j = 0; j < iCnt * 2 + 1; j++) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
 	return 0;
 }
