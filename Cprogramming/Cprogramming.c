0376bbb4ccdc4a3a7f5aefaaa1a1a05bc442844e
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-22 15:16:27 +0900

    ➕[Feat] getch( )/getche( ) 함수

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,18 +1,20 @@
 ﻿// <stdio.h> : 핵심 입력과 출력 함수들을 정의
 #include <stdio.h>
+#include <conio.h>
 
 void main()
 {
-	// getchar( ) 함수가 호출되었을 땐 
-	// 키보드에 대한 입출력 버퍼가 비어 있는 상태
 	char ch;
-	ch = getchar();
-	// putchar( )  문자를 모니터 화면에 출력하는 함수
-	putchar(ch);
-	ch = getchar();
-	putchar(ch);
-	ch = getchar();
-	putchar(ch);
+	// getch() 함수는 정말 문자 하나만을 입력받는다
+	// 표준 입력 장치의 파일 버퍼를 거치지도 않는다
+	ch = _getch();
+	printf("%c\n", ch);
+
+	// getche( ) 함수는 마치 메아리 (echo) 처럼 
+	// 사용자가 입력한 키보드의 값을 화면에 출력
+	ch = _getche();
+	printf("%c\n", ch);
 }
 
-// TES
+// a
+// bb 
