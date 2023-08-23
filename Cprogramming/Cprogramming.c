70ea426d48d613415ebdc64bb02b68b98cf9ffa4
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-23 09:40:53 +0900

    ➕[Feat] 형변환 연산자

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,24 +1,47 @@
 ﻿// <stdio.h> : 핵심 입력과 출력 함수들을 정의
 #include <stdio.h>
 
-void main()
+void main(void)
 {
-	// 부호를 표현하는 형식 활용
-	int nData1 = 1234, nData2 = -4567;
+	char ch = 'A';
+	int nData = 15;
+	float fData1 = 1.5f, fData2 = 3.4f;
 
-	printf("%d\n", nData1);
-	printf("%+d, %+d\n", nData1, nData2);
+	// 자료형이 char인 값을 int형으로 승격하고 나서 
+	printf("%d\n", ch + 2);
+	// 정수형 상수 2와 더한 결과를 각각의 형식으로 출력
+	printf("%c\n", ch + 2);
 
-	printf("%07d\n", nData1);
-	printf("%7d\n", nData1);
-	printf("%-7d\n", nData1);
+	// float형과 int형 간의 연산 결과를 다시 float형과 연산
+	printf("%f\n", fData1 * 100 / fData2);
+	// float형과 int형 그리고 float형 간의 연산 결과는 float형 
+	printf("%f\n", fData2 / fData1);
+
+	// int형 간의 연산 결과는 int형
+	// int형은 %f 형식 문자로 출력할 수 없다
+	printf("%f\n", nData * 100 / 100);
+
+	// int형 간의 연산 결과는 int형이므로 소수점 이하 정보는 절사
+	// nData 변수를 10으로 나눈 결과는 1.5가 아니라 int형인 1
+	printf("%f\n", nData / 10 * 100.0f);
+
+	// int형인 nData 변수를 일시적으로 float형으로 형변환, int형과 연산
+	// float형 간의 연산이 이루어졌으나 결과를 다시 int형으로 강제 형변환하여 정	수
+	printf("%d\n", (int)((float)nData / 10 * 100.0f));
+
+	// int형을 일시적으로 float형으로 강제 형변환을 하고서 int형과 연산
+	printf("%f\n", (float)nData * 100 / 100);
+
+	// int형 간의 연산 결과를 float형으로 형변환하여 int형과 연산
+	printf("%f\n", (float)(nData * 100) / 100);
 }
 
-/*
-1234
-+1234, -4567
-0001234
-   1234
-1234
-계속하려면 아무 키나 누르십시오 . . .
-*/
+// 67
+// C
+// 44.117646
+// 2.266667
+// 0.000000
+// 100.000000
+// 150
+// 15.000000
+// 15.000000
