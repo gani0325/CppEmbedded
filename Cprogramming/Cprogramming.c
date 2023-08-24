dcbe55cfa0b8d7909c8a9998eeebbe15e612700d
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-24 11:55:41 +0900

    ➕[Feat] 4장 연습문제 2번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,11 +2,10 @@
 
 void main()
 {
-	// getchar( ) 함수를 이용하여 세 글자 이상의 문자열을 입력받고서
-	// 그중 두 번째 문자를 printf() 함수로 화면에 출력
-	char ch;
-	ch = getchar();
-	ch = getchar();
-	printf("%c", ch);
-	ch = getchar();
+	// gets( ) 함수로 문자열을 입력받고서 printf( ) 함수를 호출하여 같은 문자열을 화면에 출력하세요
+	// 단, 문자열의 최대 길이는 영문 32자 미만이라고 가정합니다.
+	char ch[32] = { 0, };
+	gets(ch);
+
+	printf(ch);
 }
