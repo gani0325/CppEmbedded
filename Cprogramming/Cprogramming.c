cc12f883195020997b4e3c8bad9e30cbfae811ef
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-24 11:41:46 +0900

    ➕[Feat] 전달된 주소에 저장할 문자열의 구분자로 빈칸을 사용

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,15 +1,23 @@
-﻿// <stdio.h> : 핵심 입력과 출력 함수들을 정의
-#include <stdio.h>
+﻿#include <stdio.h>
 
 void main()
 {
-	int nInput = 0;
-	printf("Input number: ");
-	// scanf_s() 함수 : 형식 문자열을 사용하지만 이어지는 가변 인자로는 전부 주소가 와야 한다
-	// → 사용자의 입력을 인자로 전달 받은 주소에 저장한다는 뜻
-	scanf_s("%d", &nInput);
-	printf("%d\n", nInput);
+	char szBuffer[128] = { 0 };
+	printf("Input string: ");
+
+	// 두번째 칸부터 받을 수 있음
+	scanf_s("%s", szBuffer+1, 128);
+	puts(szBuffer);
+	puts(szBuffer+1);
 }
 
-//Input number: 10 
-//10
+//Input string: 이것은테스트문자열입니다.
+//이것은테스트문자열입니다.
+//계속하려면 아무 키나 누르십시오 . . .
+
+/**********/
+
+
+//Input string: 이것은 테스트 문자열입니다.
+//이것은
+//계속하려면 아무 키나 누르십시오 . . .
