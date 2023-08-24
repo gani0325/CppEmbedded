8f03f039900e71533ea5efb7bfe9c4bd2ae5acda
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-24 17:12:40 +0900

    ➕[Feat] 5장 연습문제 2번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -4,15 +4,11 @@
 
 void main(void)
 {
-	// 사용자로부터 정수 두 개를 입력받고 사칙 연산(덧셈, 뺄셈, 곱셈, 나눗셈) 결과를 출력
-	// 예를 들어 10과 5를 입력받았다면 15, 5, 50, 2가 출력되어야 합니다.
-	int num1, num2;
+	// 사용자로부터 정수를 입력받아 5로 나눈 나머지를 출력하는 프로그램을 작성하세요
+	int num;
 
-	printf("숫자 입력 하세용 (num1 num2) : ");
-	scanf_s("%d %d", &num1, &num2);
+	printf("숫자 입력 하세용 (num1) : ");
+	scanf_s("%d", &num);
 
-	printf("%d + %d = %d", num1, num2, num1 + num2);
-	printf("\n%d - %d = %d", num1, num2, num1 - num2);
-	printf("\n%d * %d = %d", num1, num2, num1 * num2);
-	printf("\n%d / %d = %d", num1, num2, num1 / num2);
+	printf("%d %% 5 = %d", num, num % 5);
 }
