eac75a6b8b0b8c9219bdbd62659598327cda1a0f
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-25 16:37:17 +0900

    ➕[Feat] 7장 연습문제 2번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,30 +2,15 @@
 
 void main(void)
 {
-	// 사용자로부터 영문 한 글자와 정수 둘을 입력받은 후, 영문 한 글자가 무엇인지에 따라
-	// 다음과 같은 연산을 수행하고 결과를 출력하는 프로그램을 작성하세요.
-	//	a : 두 수를 더함
-	//	b : 두 수를 곱함
-	//	c : 먼저 입력된 정수에서 나중에 입력한 정수를 뺌
-	// 만일 이중 어느 것에도 해당하지 않는 영문자가 입력되었다면, 다음처럼「알 수 없는 명령」
-	// 이라는 문자열을 출력하고서 프로그램은 종료되어야 합니다
+	// 사용자가 입력한 문자가 y이거나 Y이면 프로그램을 종료하고 아니면 계속해서 문자 하나를 입력받는 프로그램을 작성하세요.
+	// 단, if 문과 goto 문을 사용하세요
 
 	char ch;
-	int num1 = 0, num2 = 0;
-	printf("영문 한 글자와 정수 둘을 입력하세용 (ch num1 num2) : ");
-	scanf_s("%d %d %c", &num1, &num2, &ch);
+	more:
+	ch = getchar();
 
-	if (ch == 'a') {
-		printf("%d + %d = %d", num1, num2, num1 + num2);
+	if (ch == 'y' || ch == 'Y') {
+		printf("end");
 	}
-	else if (ch == 'b') {
-		printf("%d * %d = %d", num1, num2, num1 * num2);
-	}
-	else if (ch == 'c') {
-		printf("%d - %d = %d", num1, num2, num1 - num2);
-	}
-	else {
-		printf("「알 수 없는 명령」");
-	}
+	goto more;
 }
-
