8fd225aee01a4410408e8a2642cee6743a02c180
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-25 12:06:57 +0900

    ➕[Feat] sizeof : 자료형, 상수, 식별자에 대하여 메모리의 크기

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,18 +1,11 @@
-﻿// 연산의 우선순위가 달라져서 결과가 달라짐
-
-#include <stdio.h>
+﻿#include <stdio.h>
 
 void main(void)
 {
-	// 사용자로부터 정수를 입력받고서 오른쪽 3비트의 값만 0으로 초기화하여 출력하는 프로그램을 작성하세요.
-	// 예를 들어 사용자가 15를 입력했다면 8을 출력해야 합니다.
-	// 단, 절대로 뺄셈 연산자를 사용하지 말고 AND 연산자(&)를 사용해야 합니다.
+	char* szName1[16] = { 'lee' };
+	char szName2 = { 'lee' };
 
-	int num;
-
-	printf("숫자 입력 하세용 (num1) : ");
-	scanf_s("%d", &num);
-
-	// 0b 2진수로 비트 연산하기
-	printf("%d ", (num & 0b1000));
+	printf("%d\n", sizeof(char*));		// 8
+	printf("%d\n", sizeof(szName1));	// 128
+	printf("%d\n", sizeof(szName2));	// 1
 }
