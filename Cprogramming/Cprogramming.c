b14e3ec1b0bc3c25d0db25a41fa794ca89520dbb
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-25 15:22:44 +0900

    ➕[Feat] 6장 연습문제 1번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,10 +2,17 @@
 
 void main(void)
 {
-	char* szName1[16] = { 'lee' };
-	char szName2 = { 'lee' };
+	// 사용자로부터 입력받은 두 정수가 모두 10보다 크면 1을 출력하고 
+	// 그렇지 않은 경우(둘중 하나라도 10보다 작거나 같은 경우) 0을 출력하는 프로그램을 작성하세요
 
-	printf("%d\n", sizeof(char*));		// 8
-	printf("%d\n", sizeof(szName1));	// 128
-	printf("%d\n", sizeof(szName2));	// 1
+	int num1, num2;
+	printf("숫자 입력하세용 (num1, num2) : ");
+	scanf_s("%d %d", &num1, &num2);
+
+	if (num1 > 10 && num2 > 10) {
+		printf("%d", 1);
+	}
+	else {
+		printf("%d", 0);
+	}
 }
