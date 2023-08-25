864ab7b3f0a41e09f9160a504eeda3f36f8e3f90
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-25 15:56:17 +0900

    ➕[Feat] 7장 연습문제 2번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,17 +2,20 @@
 
 void main(void)
 {
-	// if 문을 사용하여 사용자로부터 입력받은 다섯 정수 중 최댓값을 출력하는 프로그램을 작성하세요.
-	// 단, 다섯 개의 정수를 한 줄로 한 번에 입력받는 것이 아니라 5회에 걸쳐서 입력받는 형태로 작성하세요.
+	// 사용자가 입력한 문자가 y이거나 Y이면 프로그램을 종료하고 아니면 계속해서 문자 하나를 입력받는 프로그램을 작성하세요.
+	// 단, if 문과 goto 문을 사용하세요
+	
+	char ch;
+	scanf_s("%c", &ch);
 
-	int num, max = 0;
-
-	for (int i = 0; i < 5; i++) {
-		scanf_s("%d", &num);
-		if (max < num) {
-			max = num;
-		}
+	if (ch == 'y' || ch == 'Y') {
+		goto END;
+	}		
+	
+	else {
+		scanf_s("%c", &ch);
 	}
 
-	printf("가장 큰 숫자는 : %d", max);
+END :
+	printf("end");
 }
