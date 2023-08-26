556fabb0d4eb7327655801b8885e19e3232580d3
gani0325 <lge0325@bme.gachon.ac.kr>
2023-08-26 10:42:17 +0900

    ➕[Feat] 단항 증감 연산자

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,16 +1,25 @@
-﻿#include <stdio.h>
+﻿// 연산의 우선순위가 달라져서 결과가 달라짐
+
+#include <stdio.h>
 
 void main(void)
 {
-	// 사용자가 입력한 문자가 y이거나 Y이면 프로그램을 종료하고 아니면 계속해서 문자 하나를 입력받는 프로그램을 작성하세요.
-	// 단, if 문과 goto 문을 사용하세요
+	int nData = 10, nNewData = 20;
+	int nResult = 0;
 
-	char ch;
-	more:
-	ch = getchar();
+	printf("%d\n", ++nData);
 
-	if (ch == 'y' || ch == 'Y') {
-		printf("end");
-	}
-	goto more;
+	// nData 변수에 저장된 값을 출력시키고서 
+	// nData 변수에 저장된 값을 1만큼 증가
+	printf("%d\n", nData++);
+
+	// nData 변수에 저장된 값을 1만큼 증가시키고서 
+	// nNewData 변수에 저장된 값과 더하는 연산을 수행
+	// 결과를 nResult 변수에 저장하고서 nNewData 변수의 값을 1만큼 증가
+	nResult = ++nData + nNewData++;
+	printf("%d\n", nResult);
 }
+
+// 11
+// 11
+// 33
