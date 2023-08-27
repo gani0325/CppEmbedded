78e05c2f8e8a6c0c34e1a7bf3654859a5fa1a6aa
gani0325 <lge0325@bme.gachon.ac.kr>
2023-08-27 23:27:55 +0900

    ➕[Feat] 배열의 이름을 포인터 변수에 담아서 사용

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,36 +1,16 @@
-﻿// 주소를 계산하는 것뿐만 아니라 
-// 계산된 주소를 간접 지정하여 문자를 출력
+﻿// 배열의 이름을 포인터 변수에 담아서 사용
 
 #include <stdio.h>
-#include <malloc.h>
-#include <string.h>
 
 void main(void)
 {
-	// char형 포인터 변수 pszData를 선언, 문자열 상수의 주소로 초기화
-	char* pszData = "Test string!";
+	char szName[16] = { 0 };
+	char* pszName = szName;
 
-	// pszData라는 포인터 변수에 저장된 주소
-	// 그 주소를 간접 지정한 메모리의 내용을 각각 주소와 문자 형식으로 출력
-	printf("[%p] %c\n", pszData, *pszData);
-	printf("[%p] %c\n", pszData + 1, *(pszData + 1));
-	printf("[%p] %c\n", pszData + 2, *(pszData + 2));
-	printf("[%p] %c\n", pszData + 3, *(pszData + 3));
-
-	// pszData라는 포인터 변수에 저장된 주소와 옵셋을 수행하여 그 주소를 출력
-	// 옵셋 결과에 해당하는 주소를 간접 지정하여 메모리에 저장된 값을 출력
-	printf("[%p] %c\n", pszData + 5, *(pszData + 5));
-	printf("[%p] %c\n", pszData + 11, *(pszData + 11));
-
-	printf("%c %c\n", *pszData + 1, *pszData + 2);
+	sprintf(pszName, "%s", "홍길동");
+	puts(szName);
 }
 
 /*
-[00007FF610729C28] T
-[00007FF610729C29] e
-[00007FF610729C2A] s
-[00007FF610729C2B] t
-[00007FF610729C2D] s
-[00007FF610729C33] !
-U V
+홍길동
 */
