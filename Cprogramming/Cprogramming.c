99f26c28917ce6605c529b566ffa54b8889f2aaf
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-28 12:21:09 +0900

    ➕[Feat] 8장 연습문제 2번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,31 +2,20 @@
 
 void main(void)
 {
-	// 사용자로부터 다섯 개의 정수를 입력받고, 
-	// 그 중 가장 큰 수와 가장 작은 수를 골라서 출력하는 프로그램을 작성하세요
-
-	int max = -1e9, min = 1e9;
-	int nInput = 0;
-
-	for (int i = 0; i < 5; ++i)
-	{
-		scanf_s("%d", &nInput);
-		if (nInput > max) {
-			max = nInput;
-		}
-		else if (nInput < min) {
-			min = nInput;
+	// 10~100까지의 숫자 중에서 4의 배수가 몇 개이며, 
+	// 이들의 총합이 얼마인지 알아내어 출력하는 프로그램을 작성하세요
+	int sum = 0;
+	int cnt = 0;
+	for (int i = 10; i <= 100; i++) {
+		if (i % 4 == 0) {
+			sum += i;
+			cnt++;
 		}
 	}
 
-	printf("가장 큰 수 : %d, 가장 작은 수 : %d", max, min);
+	printf("%d %d", sum, cnt);
 }
 
 /*
-4
-5
-7
-1
-6
-가장 큰 수 : 7, 가장 작은 수 : 1
+1288 23
 */
