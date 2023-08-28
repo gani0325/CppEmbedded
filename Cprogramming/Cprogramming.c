5c4531ec3f6363ba7380c14e3a1ac64bc040852d
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-28 11:39:33 +0900

    ➕[Feat] 8장 연습문제 1번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,31 +2,16 @@
 
 void main(void)
 {
-	int i = 0, nInput = 0, nTotal = 0;
+	// 1~100까지의 합을 구하는 프로그램을 작성하세요
+	int sum = 0;
 
-	for (i = 0; i < 5; ++i)
-	{
-		scanf_s("%d", &nInput);
-
-		if (nInput < 0)
-			continue;
-
-		nTotal += nInput;
+	for (int i = 1; i <= 100; i++) {
+		sum += i;
 	}
 
-	printf("Total: %d, i: %d\n", nTotal, i);
+	printf("%d", sum);
 }
-/*
-1
-2
-3
-4
--5 //실행 흐름은 조건식이 아니라 반복 실행식으로 이동되었다.
-Total: 10, i: 5 //음수를 저장하지않음.
-계속하려면 아무 키나 누르십시오 . . .
 
-➡️ break문을 만난다면 반복문을 종료한다.
-➡️ continue 문을 만난다
-1) while 문의 경우 조건식으로 흐름 변경
-2) for 문의 경우 반복 실행식으로 흐름 변경
+/*
+5050
 */
