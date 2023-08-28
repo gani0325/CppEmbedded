11fff15224855e168890523ad37b728ef825fb61
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-28 11:36:18 +0900

    ➕[Feat] 중첩된 반복문과 continue문

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,17 +2,31 @@
 
 void main(void)
 {
-	int x = 0, y = 0;
-	for (y = 0; y < 5; y++)
+	int i = 0, nInput = 0, nTotal = 0;
+
+	for (i = 0; i < 5; ++i)
 	{
-		for (x = 0; x < 4; ++x)
-		{
-			if (x > 2)
-				break; // x값이 3이 되었을 때 break문이 실행된다.
+		scanf_s("%d", &nInput);
 
-			printf("*\t");
-		}
+		if (nInput < 0)
+			continue;
 
-		putchar('\n');
+		nTotal += nInput;
 	}
+
+	printf("Total: %d, i: %d\n", nTotal, i);
 }
+/*
+1
+2
+3
+4
+-5 //실행 흐름은 조건식이 아니라 반복 실행식으로 이동되었다.
+Total: 10, i: 5 //음수를 저장하지않음.
+계속하려면 아무 키나 누르십시오 . . .
+
+➡️ break문을 만난다면 반복문을 종료한다.
+➡️ continue 문을 만난다
+1) while 문의 경우 조건식으로 흐름 변경
+2) for 문의 경우 반복 실행식으로 흐름 변경
+*/
