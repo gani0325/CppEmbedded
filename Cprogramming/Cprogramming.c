b355cd58fed64a691cf500556624811dd6ed7fe9
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-28 11:17:05 +0900

    ➕[Feat] 중첩된 for 반복문

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,17 +1,23 @@
 ﻿#include <stdio.h>
-
 void main(void)
 {
-    int i = 0;
-
-    for (i = 0; i < 5; ++i)
-    {
-        printf("%dth\n", i);
-    }
-}
-
-// 0th
-// 1th
-// 2th
-// 3th
-// 4th
+	int x = 0, y = 0;
+	while (y < 5)
+	{
+		x = 0;
+		while (x < 4)
+		{
+			printf("*\t");
+			x++;
+		}
+		putchar('\n');
+		y++;
+	}
+} // x값의 증가는 열의 증가로 y값의 증가는 행의 증가를 나타낸다.
+/*
+*       *       *       *
+*       *       *       *
+*       *       *       *
+*       *       *       *
+*       *       *       *
+*/
