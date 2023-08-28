28effa9f2d839243ba8753bb0720e62c8c1bbeef
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-28 13:57:41 +0900

    ➕[Feat] 8장 연습문제 5번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,21 +1,32 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 void main(void)
 {
-	// 10~100까지의 숫자 중에서 4의 배수가 몇 개이며, 
-	// 이들의 총합이 얼마인지 알아내어 출력하는 프로그램을 작성하세요
-	int sum = 0;
+	// 사용자로부터 입력받은 문자열에서 문자 t가 몇 개나 있는지 세어서(Count) 결과를 출력하는 프로그램을 작성하세요.
+	// 단, 입력받은 문자열에 슬래시(/ )가 있으면 더 이상 세지 않고 문자열이 끝났다고 판단하며, 
+	// 슬래시(/ )가 입력될 때까지 계속해서 입력을 받습니다
+
 	int cnt = 0;
-	for (int i = 10; i <= 100; i++) {
-		if (i % 4 == 0) {
-			sum += i;
-			cnt++;
+	char ch;
+
+	while (1) {
+		ch = getchar();
+
+		if (ch == '/') {
+			break;
+		}
+		else if (ch == 't') {
+			cnt += 1;
+		}
+		else {
+			continue;
 		}
 	}
 
-	printf("%d %d", sum, cnt);
+	printf("t의 개수는 %d개", cnt);
 }
 
 /*
-1288 23
+
 */
