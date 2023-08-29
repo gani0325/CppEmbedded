5f0d04e1492c9540b13fcaa4e372c181ad50ee7e
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-29 15:33:07 +0900

    ➕[Feat] 9장 연습문제 3번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,41 +1,33 @@
-﻿// 사용자로부터 입력받은 크기만큼의 메모리를 동적으로 할당받은 후
-// 다시 문자열을 입력받아 앞서 동적으로 할당받은 메모리에 저장하고 출력
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <malloc.h>
 
 void main(void)
 {
-	// 한글 이름 두 개를 입력받은 후, 같은 성인지 비교하여 같은 성이면 '같음', 
-	// 그렇지 않으면 '다름'이라는 문자열을 출력하는 프로그램을 작성하세요.
-	// 단, 만일 사용자가 문자열을 입력하지 않았다면 무조건 '다름'이라고 출력합니다
+	// 사용자로부터 정수를 입력받은 후, 그 정수의 크기만큼(바이트 단위) 메모리를 동적으로
+	// 할당받고 0으로 초기화하는 프로그램을 작성하세요.
+	// 단, 정수의 값이 100을 초과하면 강제로 100으로 조정하고, 10보다 작은 경우에는 10으로 조정합니다.
 
-	char name1[10];
-	char name2[10];
+	int num;
+	int* pnNum = NULL;
+	scanf_s("%d", &num);
+	printf("%d", num);
 
-	//scanf_s("%c %c", name1, name2);
-	gets(name1);
-	gets(name2);
-
-	puts(name1);
-	puts(name2);
-
-	if (name1[0] == name2[0]) {
-		if (name1[0] == name2[0] && name2[0] == '\0' && name1[0] == '\0') {
-			printf("다름");
-		}
-		else {
-			printf("같음");
-		}		
+	if (num > 100) {
+		pnNum = (int*)malloc(sizeof(int) * 100);
+		memset(pnNum, 0, sizeof(int) * 100);
+	}
+	else if (num < 10) {
+		pnNum = (int*)malloc(sizeof(int) * 10);
+		memset(pnNum, 0, sizeof(int) * 10);
 	}
 	else {
-		printf("다름");
+		pnNum = (int*)malloc(sizeof(int) * num);
+		memset(pnNum, 0, sizeof(int) * num);
 	}
+
+	free(pnNum);
 }
 
 /*
-이가은
-이다은
-이가은
-이다은
-같음
+
 */
