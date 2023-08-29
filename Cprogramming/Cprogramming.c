1c91a9b91d66ed44d29e24300e538d29b36e0f15
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-29 17:10:25 +0900

    ➕[Feat] calloc로 메모리 동적 할당

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,9 +1,10 @@
 ﻿// 사용자로부터 입력받은 크기만큼의 메모리를 동적으로 할당받은 후
 // 다시 문자열을 입력받아 앞서 동적으로 할당받은 메모리에 저장하고 출력
-#define _CRT_SECURE_NO_WARNINGS
+// 메모리를 동적으로 할당받기 위해 calloc( ) 함수를 사용
+
 #include <stdio.h>
 #include <malloc.h>
-
+#include <string.h>
 
 void main(void)
 {
@@ -13,17 +14,15 @@ void main(void)
 	printf("Input length : ");
 	// 사용자로부터 정수를 입력받아 nInput 변수에 저장
 	scanf_s("%d\n", &nInput);
-	// malloc( ) 함수로 입력받은 수만큼의 메모리를 동적으로 할당받고 
-	// 그 주소를 pszData 포인터 변수에 저장
-	pszData = (char*)malloc(sizeof(char) * nInput);
+	// calloc( ) 함수를 이용하여 메모리를 동적으로 할당받고
+	// 0으로 초기화한 후, 그 주소를 pszData 포인터 변수에 저장
+	pszData = (char*)calloc(nInput, sizeof(char));
 
 	// 표준 입력 장치 파일의 입출력 정보를 초기화
-	// fflush(stdin)은 주로 개행문자 '\n'을 버리기 위해 사용
 	fflush(stdin);
 	// 사용자로부터 문자열을 입력받아서 pszData 포인터 변수에 
 	// 저장된 주소의 메모리에 저장
 	gets(pszData);
-	// fgets(pszData, sizeof(pszData), stdin);
 	puts(pszData);
 	// 동적으로 할당받았던 메모리를 free( ) 함수로 해제
 	free(pszData);
@@ -31,5 +30,5 @@ void main(void)
 
 /*
 Input length : //16
-Choi ho-sung // Choi ho-sung
+홍길동 // 홍길동
 */
