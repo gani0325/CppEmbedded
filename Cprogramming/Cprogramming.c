694a94af213548c014c1d1c0c63ae5c9154a8aa9
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-29 17:45:56 +0900

    ➕[Feat] 9장 연습문제 1번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,28 +1,41 @@
-﻿#include <stdio.h>
+﻿// 사용자로부터 입력받은 크기만큼의 메모리를 동적으로 할당받은 후
+// 다시 문자열을 입력받아 앞서 동적으로 할당받은 메모리에 저장하고 출력
+#include <stdio.h>
 #include <malloc.h>
 
 void main(void)
 {
-	//  두 정수를 입력받아 같은 숫자인지 아닌지 비교하는 프로그램을 작성하세요.
-	//  단, == 연산자가 아니라 memcmp() 함수를 사용하세요
+	// 한글 이름 두 개를 입력받은 후, 같은 성인지 비교하여 같은 성이면 '같음', 
+	// 그렇지 않으면 '다름'이라는 문자열을 출력하는 프로그램을 작성하세요.
+	// 단, 만일 사용자가 문자열을 입력하지 않았다면 무조건 '다름'이라고 출력합니다
 
-	int num1[] = { 0 };
-	int num2[] = { 0 };
-	//int* ptrnum1 = &num1, ptrnum2 = &num2;
+	char name1[10];
+	char name2[10];
 
-	scanf_s("%d %d", &num1, &num2);
+	//scanf_s("%c %c", name1, name2);
+	gets(name1);
+	gets(name2);
 
-	if (memcmp(num1, num2, 10) == 0) {
-		printf("%d %d 같은 수", *num1, *num2);
+	puts(name1);
+	puts(name2);
+
+	if (name1[0] == name2[0] && name1[1] == name2[1]) {
+		if (name1[0] == name2[0] && name2[0] == '\0' && name1[0] == '\0') {
+			printf("다름");
+		}
+		else {
+			printf("같음");
+		}
 	}
-	else if (memcmp(num1, num2, 10) == 1) {
-		printf("%d > %d", *num1, *num2);
-	}
-	else if (memcmp(num1, num2, 10) == -1) {
-		printf("%d < %d", *num1, *num2);
+	else {
+		printf("다름");
 	}
 }
 
 /*
-
+이가은
+이다은
+이가은
+이다은
+같음
 */
