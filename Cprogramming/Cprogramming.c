18cc2352e97103b746154c426e75d359c6cac8fa
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-30 14:27:04 +0900

    ➕[Feat] 2차원 배열의 주소 및 값

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,41 +1,36 @@
-﻿// 사용자로부터 입력받은 크기만큼의 메모리를 동적으로 할당받은 후
-// 다시 문자열을 입력받아 앞서 동적으로 할당받은 메모리에 저장하고 출력
-#include <stdio.h>
-#include <malloc.h>
+﻿#include <stdio.h>
 
-void main(void)
-{
-	// 한글 이름 두 개를 입력받은 후, 같은 성인지 비교하여 같은 성이면 '같음', 
-	// 그렇지 않으면 '다름'이라는 문자열을 출력하는 프로그램을 작성하세요.
-	// 단, 만일 사용자가 문자열을 입력하지 않았다면 무조건 '다름'이라고 출력합니다
+void main() {
+	char aszData[3][16] = {
+		"Test string",
+		"Sample data",
+		"Array of array"
+	};
 
-	char name1[10];
-	char name2[10];
+	printf("%c\n", aszData[1][3]);
+	printf("%c\n", *(*(aszData + 1) + 3));
 
-	//scanf_s("%c %c", name1, name2);
-	gets(name1);
-	gets(name2);
+	printf("%s\n", aszData[0]);
+	printf("%s\n", aszData + 1);
+	printf("%s\n", *(aszData + 2));
+	//printf("%p\n", aszData);
+	//printf("%p\n", aszData[0]);
+	//printf("%p\n", aszData + 1);
+	//printf("%p\n", aszData + 2);
+	
+	printf("%p\n", *aszData);
+	printf("%p\n", *aszData + 1);
+	printf("%s\n", *(aszData + 1) + 1);
 
-	puts(name1);
-	puts(name2);
-
-	if (name1[0] == name2[0] && name1[1] == name2[1]) {
-		if (name1[0] == name2[0] && name2[0] == '\0' && name1[0] == '\0') {
-			printf("다름");
-		}
-		else {
-			printf("같음");
-		}
-	}
-	else {
-		printf("다름");
-	}
+	char aszData2[16] = "hello";
+	printf("%s\n", aszData2);
 }
-
 /*
-이가은
-이다은
-이가은
-이다은
-같음
+p
+p
+Test string
+Sample data
+Array of array
+est string
+ample data
 */
