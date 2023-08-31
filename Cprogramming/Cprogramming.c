c7c36e034937326b46b600b5226b3cef8c834cfd
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-31 14:49:11 +0900

    ➕[Feat] 11장 연습문제 2번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -3,34 +3,39 @@
 
 int max = _CRT_INT_MAX;
 
-int GetLength(const char* ch) {
-	int cnt = 0;
+char GetReverse(char* ch) {
+	char reverse[16];
+	
+	/*printf("%p", ch);
+	printf("%c", *ch);
+	printf("%c", *(ch + 1));
+	printf("%c", *(ch + 2));*/
 
-	for (int i = 0; i < max; i++) {
-		if (*(ch + i) == 00) {
-			break;
-		}
-		else {
-			cnt++;
-		}
+	for (int i = 0; i < strlen(ch); i++) {
+		*(reverse + i) = *(ch + (strlen(ch) - 1 - i));
 	}
+	printf("%c", *reverse);
+	printf("%c", *(reverse + 1));
+	printf("%c", *(reverse + 2));
+	printf("%c", *(reverse + 3));
+	printf("%c", *(reverse + 4));
 
-	return cnt;
+	// printf("%p", reverse);
+
+	return reverse;
 }
 
 void main(void)
 {
-	// 인자로 char*형을 받아서 문자열의 길이를 반환하는 함수를 작성하세요. 
-	// 단, 절대로 strlen() 함수를 사용하지 말고 직접 계산하는 코드를 작성하세요.
-	// 함수의 이름은 GetLength()로 합니다
+	// 인자로 char*형을 받아서 문자열을 거꾸로 출력하는 함수를 작성하세요.
+	// 문제를 해결하기 위해서 strlen() 함수를 사용하거나 1번 문제에서 작성한 함수를 사용해도 좋습니다.
 
 	char ch[16];
 	printf("문자열 입력하세요 : ");
 	gets(ch);
-
-	int result = 0;
-	result = GetLength(ch);
-	printf("문자열 길이 : %d", result);
+	
+	GetReverse(ch);
+	//printf("문자열 거꾸로 : %c", GetReverse(ch));
 }
 
 /*
