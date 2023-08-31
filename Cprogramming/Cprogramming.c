1d3505ab6aff8f4405b5016a35981a7688198297
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-31 17:12:49 +0900

    ➕[Feat] 12장 연습문제 1번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,46 +1,20 @@
 ﻿#include <stdio.h>
 
-int UpSorted(char* array, int num) {
-	int sort[] = { 0, };
-	int min = 0;
-
-	printf("#####%p\n", array);
-	printf("^^^^^^%d", *(array+1));
-	for (int i = 0; i < num; i++) {
-		min = array[i];
-		for (int j = i + 1; j < num; j++) {
-			if (min > *(array + 4*j)) {
-				min = *(array + 4*j);
-			}
-			else {
-				continue;
-			}
-		}
-		*(sort + 4*i) = min;
-	}
-	printf("^^^^^^%p", sort);
-
-	return 0;
-}
-
 void main(void)
 {
-	// 인자로 int형 배열(혹은 주소)과 요소의 개수를 받은 후, 
-	// 오름차순으로 정렬하는 함수를 작성하세요
+	// 문자열을 입력받아서 대문자는 소문자로, 
+	// 소문자는 대문자로 변환하여
+	// 출력하는 프로그램을 작성하세요
 
-	int Arr[] = {0,};
-	int num = 0;
-	int n = 0;
+	char ch[15] = { 0, };
+	gets(ch);
 
-	scanf_s("%d", &num);
-	for (int i = 0; i < num; i++) {
-		scanf_s("%d", &n);
-		Arr[i] = n;
+	for (int i = 0; i < strlen(ch); i++) {
+		printf("%c", toupper(*(ch + i)));
 	}
+	printf("\n");
 
-	//printf("#####%d", *Arr);
-	printf("#####%p", &Arr);
-	printf("^^^^^^%d", *Arr);
-	UpSorted(Arr, num);
-
+	for (int i = 0; i < strlen(ch); i++) {
+		printf("%c", tolower(*(ch + i)));
+	}
 }
