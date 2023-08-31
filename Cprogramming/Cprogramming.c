67f9f4aa9fa24059ff1cdc387ce57b4d58df79f0
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-31 12:09:31 +0900

    ➕[Feat] 10장 연습문제 6번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -3,61 +3,59 @@
 
 void main(void)
 {
-	int input;
-	printf("배열의 사이즈 : ");
-	scanf_s("%d", &input);
+	// 다음과 같이 배열을 채우는 프로그램을 작성한 후, 
+	// 맨 오른쪽 열과 맨 아래 행에 각각 행과 열의 합을 저장하는 프로그램을 작성하세요
+
+	int row, col;
+	printf("가로 세로 사이즈 : ");
+	scanf_s("%d %d", &row, & col);
 
 	//size만큼 배열의 세로 동적할당
-	int** Array = (int**)malloc(sizeof(int*) * input);
+	int** Array = (int**)malloc(sizeof(int*) * col);
 
 	//size만큼 배열의 가로 동적할당 
-	for (int i = 0; i < input; i++) {
-		Array[i] = (int*)malloc(sizeof(int) * input);
+	for (int i = 0; i < col; i++) {
+		Array[i] = (int*)malloc(sizeof(int) * row);
 	}
-
-	//int k = 1;
-	//for (int i = 0; i < input; i++) {
-	//	for (int j = 0; j < input; j++) {
-	//		Array[i][j] = k;
-	//		k++;
-	//	}
-	//}
 
 	int k = 1;
-	int center = input / 2;
+	int colSum = 0;
 
-	for (int i = 0; i < center; i++) {
-		for (int j = input - i - 1; j >= i; j--) {
+	for (int i = 0; i < row - 1; i++) {
+		colSum = 0;
+		for (int j = 0; j < col - 1; j++) {
 			Array[i][j] = k;
 			k++;
+			colSum += Array[i][j];
+			//printf("%d\n", colSum);
 		}
-		for (int j = i + 1; j < input - i; j++) {
-			Array[j][i] = k;
-			k++;
-		}
-		for (int j = i + 1; j < input - i; j++) {
-			Array[input - 1 - i][j] = k;
-			k++;
-		}
-		for (int j = input - 2 - i; j >= 1 + i; j--) {
-			Array[j][input - 1 - i] = k;
-			k++;
-		}
+		Array[i][col-1] = colSum;
 	}
 
-	Array[center][center] = input * input;
+	int rowSum = 0;
+	for (int i = 0; i < col; i++) {
+		rowSum = 0;
+		for (int j = 0; j < row - 1; j++) {
+			rowSum += Array[j][i];
+		}
+		Array[row - 1][i] = rowSum;
+	}
 
-	for (int i = 0; i < input; i++) {
-		for (int j = 0; j < input; j++) {
+
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
 			printf("%d\t", Array[i][j]);
 		}
 		printf("\n");
 	}
 
 	//메모리 해제
-	for (size_t i = 0; i < input; i++)
+	for (size_t i = 0; i < row; i++)
 	{
-		free(Array[i]);
+		for (size_t j = 0; j < col; j++) {
+			free(Array[i][j]);
+		}
 	}
 	free(Array);
 }
