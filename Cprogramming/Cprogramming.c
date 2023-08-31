4cc929d48a53ce462bd3074b0792e0b773776cb3
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-31 17:38:43 +0900

    ➕[Feat] 12장 연습문제 4번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -2,21 +2,14 @@
 
 void main(void)
 {
-	// 사용자로부터 두 문자열을 입력받은 후, 
-	// 하나로 합쳐서 출력하는 프로그램을 작성하세요.
-	// 단, 두 문자열을 합칠 때 반드시 sprintf() 함수를 사용해야 하며,
-	// 첫 번째 입력받은 문자열이 저장된 메모리(배열)에 합친 결과가 저장되도록 합니다.
+	// 3번 문제의 답을 수정하여 sprintf( ) 함수 대신 strcat( ) 함수를 사용하여
+	// 같은 결과가 나오도록 코드를 변경하세요.
 
-	char ch1[] = { 0, };
-	char ch2[] = { 0, };
+	char ch1[15] = { 0, };
+	char ch2[15] = { 0, };
 	gets(ch1);
 	gets(ch2);
-	printf("\n%d", strlen(ch1));
-	printf("%d", strlen(ch2));
-	printf("%p", &ch1[sizeof(strlen(ch1))]);
-	//strcat(ch1, ch2);
-	//puts(ch1);
 
-	sprintf_s(&ch1[sizeof(strlen(ch1))] + 1, strlen(ch2) + 1, "%s", ch2);
-
+	strcat(ch1, ch2);
+	puts(ch1);
 }
