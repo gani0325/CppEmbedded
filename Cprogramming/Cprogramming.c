208702ca9954ef381615055888f244548d8a6ea4
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-31 09:42:21 +0900

    ➕[Feat] 10장 연습문제 5번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -3,9 +3,9 @@
 
 void main(void)
 {
-	int input=7;
+	int input;
 	printf("배열의 사이즈 : ");
-	//scanf_s("%d", &input);
+	scanf_s("%d", &input);
 
 	//size만큼 배열의 세로 동적할당
 	int** Array = (int**)malloc(sizeof(int*) * input);
@@ -14,7 +14,7 @@ void main(void)
 	for (int i = 0; i < input; i++) {
 		Array[i] = (int*)malloc(sizeof(int) * input);
 	}
-	
+
 	//int k = 1;
 	//for (int i = 0; i < input; i++) {
 	//	for (int j = 0; j < input; j++) {
@@ -27,33 +27,33 @@ void main(void)
 	int center = input / 2;
 
 	for (int i = 0; i < center; i++) {
-		for (int j = i; j < input - i;j++) {
+		for (int j = input - i - 1; j >= i; j--) {
 			Array[i][j] = k;
 			k++;
 		}
 		for (int j = i + 1; j < input - i; j++) {
-			Array[j][input - 1 - i] = k;
+			Array[j][i] = k;
 			k++;
 		}
-		for (int j = input - 2 - i; j >= i; j--) {
+		for (int j = i + 1; j < input - i; j++) {
 			Array[input - 1 - i][j] = k;
 			k++;
 		}
-		for (int j = 1 + i; j <= input - 2 - i; j++) {
-			Array[input - 1 - j][i] = k;
+		for (int j = input - 2 - i; j >= 1 + i; j--) {
+			Array[j][input - 1 - i] = k;
 			k++;
 		}
 	}
 
 	Array[center][center] = input * input;
-	
+
 	for (int i = 0; i < input; i++) {
 		for (int j = 0; j < input; j++) {
 			printf("%d\t", Array[i][j]);
 		}
 		printf("\n");
 	}
-	
+
 	//메모리 해제
 	for (size_t i = 0; i < input; i++)
 	{
@@ -63,5 +63,12 @@ void main(void)
 }
 
 /*
-
+배열의 사이즈 : 7
+7       6       5       4       3       2       1
+8       29      28      27      26      25      24
+9       30      43      42      41      40      23
+10      31      44      49      48      39      22
+11      32      45      46      47      38      21
+12      33      34      35      36      37      20
+13      14      15      16      17      18      19
 */
