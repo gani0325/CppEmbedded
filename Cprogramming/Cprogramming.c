d3ac0ffe3198aceca4daa92e990252d2ead780bf
가은 이 <lge0325@bme.gachon.ac.kr>
2023-08-31 17:35:09 +0900

    ➕[Feat] 12장 연습문제 3번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -3,17 +3,20 @@
 void main(void)
 {
 	// 사용자로부터 두 문자열을 입력받은 후, 
-	// 두 문자열 중에서 길이가 긴 문자열이 출력되는 프로그램을 작성하세요
+	// 하나로 합쳐서 출력하는 프로그램을 작성하세요.
+	// 단, 두 문자열을 합칠 때 반드시 sprintf() 함수를 사용해야 하며,
+	// 첫 번째 입력받은 문자열이 저장된 메모리(배열)에 합친 결과가 저장되도록 합니다.
 
-	char ch1[15] = { 0, };
-	char ch2[15] = { 0, };
+	char ch1[] = { 0, };
+	char ch2[] = { 0, };
 	gets(ch1);
 	gets(ch2);
+	printf("\n%d", strlen(ch1));
+	printf("%d", strlen(ch2));
+	printf("%p", &ch1[sizeof(strlen(ch1))]);
+	//strcat(ch1, ch2);
+	//puts(ch1);
 
-	if (strlen(ch1) > strlen(ch2)) {
-		puts(ch1);
-	}
-	else {
-		puts(ch2);
-	}
+	sprintf_s(&ch1[sizeof(strlen(ch1))] + 1, strlen(ch2) + 1, "%s", ch2);
+
 }
