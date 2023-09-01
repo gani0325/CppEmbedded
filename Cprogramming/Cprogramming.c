9b5cf053cfc803a2656a673d321e50ebc601d26c
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-01 17:46:09 +0900

    ➕[Feat] 재귀함수를 이용한 피보나치 수열

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,18 +1,27 @@
 ﻿#include <stdio.h>
 
-int TestFunc1(int nParam) { return nParam * 2; }
-int TestFunc2(int nParam) { return nParam * 3; }
+int fibo(int i) {
+    if (i == 0) {
+        return 0;
+    }
+    if (i == 1 || i == 2) {
+        return 1;
+    } 
+    return fibo(i - 1) + fibo(i - 2);
+}
 
+int max = _CRT_INT_MAX;
 void main()
 {
-    int nInput = 0;
-    // 함수 포인터
-    int (*pfTestFunc)(int) = NULL;
+    // 재귀 호출을 이용하여 값이 60 이하인 
+    // 피보나치(Fibonacci) 수열을 출력하는 프로그램을 작성하세요.
+    // 참고로 피보나치 수열은 다음과 같이 앞뒤 숫자의 합이 이어지는 모습을 하고 있습니다+
+    // 1 1 2 3 5 8 13 21 34 55  
 
-    scanf_s("%d", &nInput);
-    if (nInput > 5)
-        pfTestFunc = TestFunc1;
-    else
-        pfTestFunc = TestFunc2;
-    printf("%d\n", pfTestFunc(nInput));
+    for (int j = 1; j < 60; j++) {
+        if (fibo(j) > 60) {
+            break;
+        }
+        printf("%d ", fibo(j));
+    }
 }
