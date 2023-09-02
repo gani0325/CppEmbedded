7bdf00094969855b035b21a45d398543234ea97d
gani0325 <lge0325@bme.gachon.ac.kr>
2023-09-03 00:42:07 +0900

    ➕[Feat] int형 매개 변수가 둘인 함수에 대하여 포인터 변수

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,29 +1,22 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
-void PutString(char* str) {
-    if (*str == NULL) {
-        return;
-    }
-        
-    else
-    {   
-        PutString(str + 1);
-        printf("%c", *str);
-    }
+int temp(int *a, int *b) {
+    int* ptrA;
+    int* ptrB;
+
+    
 }
 
 void main()
 {
-    // 인자로 char*형을 받아서 문자열을 거꾸로 출력하는 재귀 호출 함수를 작성하세요.
-    // 함수의 원형은「void PutString(char* pszData); 」와 같습니다
+    // char*형을 반환하고 int형 매개 변수가 둘인 함수에 대하여 포인터 변수를 선언하세요.
+    int *a;
+    int *b;
+    gets(a);
+    gets(b);
 
-    char* pszData[20] = { 0, };
-
-    gets(pszData);
-    puts(pszData);
-
-    PutString(pszData);
+    temp(a, b);
 }
 
 /*
