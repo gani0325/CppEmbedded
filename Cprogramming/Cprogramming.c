1bf411f334c934e31560105be104065879532f10
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-04 09:36:37 +0900

    ➕[Feat] 15장 연습문제 1번

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,26 +1,27 @@
 ﻿#include <stdio.h>
-#include <stdlib.h>
 
-int temp(int *a, int *b) {
-    int* ptrA;
-    int* ptrB;
-
-    
+int fibo(int i) {
+    if (i == 0) {
+        return 0;
+    }
+    if (i == 1 || i == 2) {
+        return 1;
+    }
+    return fibo(i - 1) + fibo(i - 2);
 }
 
+int max = _CRT_INT_MAX;
 void main()
 {
-    // char*형을 반환하고 int형 매개 변수가 둘인 함수에 대하여 포인터 변수를 선언하세요.
-    int *a;
-    int *b;
-    gets(a);
-    gets(b);
+    // 재귀 호출을 이용하여 값이 60 이하인 
+    // 피보나치(Fibonacci) 수열을 출력하는 프로그램을 작성하세요.
+    // 참고로 피보나치 수열은 다음과 같이 앞뒤 숫자의 합이 이어지는 모습을 하고 있습니다+
+    // 1 1 2 3 5 8 13 21 34 55  
 
-    temp(a, b);
+    for (int j = 1; j < 60; j++) {
+        if (fibo(j) > 60) {
+            break;
+        }
+        printf("%d ", fibo(j));
+    }
 }
-
-/*
-hello
-hello
-olleh
-*/
