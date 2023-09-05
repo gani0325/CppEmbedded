5c67cd48b0ecfe5fdba4ea94fb3a9d2766f56564
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-05 12:13:58 +0900

    🚧[Refactor] malloc  으로 메모리 할당 대체하기

--- Cprogramming/LibScamper/pch.c
+++ Cprogramming/LibScamper/pch.c
@@ -3,15 +3,16 @@
 #include "pch.h"
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
+
 // 미리 컴파일된 헤더를 사용하는 경우 컴파일이 성공하려면 이 소스 파일이 필요합니다.
 
-
-char* szBuffer[20] = { 0 };
 char* Substitute(char* const  orgin, char* source, char* target) {
-	
-	char temp[20] = { 0 };
+	char * szBuffer = malloc(strlen(orgin) * sizeof(orgin) + 1);
+	memset(szBuffer, 0, strlen(orgin) * sizeof(orgin) + 1);
+
 	char* source_ptr = orgin;
-	char* target_ptr = temp;
+	char* target_ptr = szBuffer;
 
 	while (*source_ptr) {
 
@@ -31,8 +32,6 @@ char* Substitute(char* const  orgin, char* source, char* target) {
 	}
 	target_ptr = "/0";
 
-	//szBuffer = malloc(strlen(temp) + 1);
-	memcpy(szBuffer, temp, 20);
 	puts(szBuffer);
 	return szBuffer;
 }
