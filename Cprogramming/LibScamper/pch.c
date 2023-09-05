1bb04a90f5798b8d004d285bf6b3e46d6b9e4642
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-05 16:32:35 +0900

    ➕[Feat] sCamper 문자열 합치기

--- Cprogramming/LibScamper/pch.c
+++ Cprogramming/LibScamper/pch.c
@@ -6,32 +6,12 @@
 #include <malloc.h>
 
 // 미리 컴파일된 헤더를 사용하는 경우 컴파일이 성공하려면 이 소스 파일이 필요합니다.
+// 함수 정의
+char* Combine(char* const  sourceA, char* const  sourceB) {
+	char* szBuffer = malloc((strlen(sourceA) + strlen(sourceB)) * sizeof(sourceA) + 1);
+	memset(szBuffer, 0, (strlen(sourceA) + strlen(sourceB)) * sizeof(sourceA) + 1);
 
-char* Substitute(char* const  orgin, char* source, char* target) {
-	char * szBuffer = malloc(strlen(orgin) * sizeof(orgin) + 1);
-	memset(szBuffer, 0, strlen(orgin) * sizeof(orgin) + 1);
+	szBuffer = strcat(sourceA, sourceB);
 
-	char* source_ptr = orgin;
-	char* target_ptr = szBuffer;
-
-	while (*source_ptr) {
-
-		// 1) 문자열 5개씩 비교하기	
-		if (strncmp(source_ptr, source, strlen(source)) == 0) {
-			strcpy(target_ptr, target);
-			// 2) target 을 copy 하고 그다음 살펴볼 곳으로 포인터 이동
-			target_ptr += strlen(target);
-			source_ptr += strlen(source);
-		}
-		else {
-			strncpy(target_ptr, source_ptr, 1);
-			target_ptr++;
-			source_ptr++;
-		}
-
-	}
-	target_ptr = "/0";
-
-	puts(szBuffer);
 	return szBuffer;
 }
--- Cprogramming/LibScamper/pch.h
+++ Cprogramming/LibScamper/pch.h
@@ -1,3 +1,3 @@
 ﻿#pragma once
 // 함수 선언
-char* Substitute(char* const  orgin, char* source, char* target);
+char* Combine(char* const  sourceA, char* const  sourceB);
