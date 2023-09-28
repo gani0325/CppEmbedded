0bdcf3c9a176dafc97f81cc5ff36e79f09ef3221
gani0325 <lge0325@bme.gachon.ac.kr>
2023-09-28 23:39:37 +0900

    ➕[Feat] Call by value와 Call by reference

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,15 +1,44 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+// 포인터의 장점은 어느곳에 있든지 주소를 직접적으로 참조함으로써 값을 가져올 수 있고 변경할 수 있다
+
+// Call by value
+// 함수에서 값을 복사해서 전달하는 방식으로, 인자로 전달되는 변수를 함수의 매개변수에 복사한다. 
+// 원본 값을 바꿀 필요가 없는 경우
+void swap1(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+// Call by reference
+// 함수에서 값을 전달하는 대신 주소값을 전달하는 방식
+void swap2(int* a, int* b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main() {
-    int a[] = { 10,20,30,40,50 };
-    printf("배열 a[0]의 주소값 = %p \n", a);
+    int a = 10, b = 50;
 
-    // a+i는 a가 포인터이므로 배열 시작 주소에 (i*배열요소의 크기)이 더해짐
-    printf("배열 a[1]의 주소값 = %p \n", a + 1);
-    printf("배열 a[0]의 값 = %d \n", *a);
+    printf("call by value\n");
+    swap1(a, b);
+    printf("결과 a : %d, b : %d\n\n", a, b);
 
-    // a+i는 &a[i]와 같고 또한 *(a+i)는 a[i]와 완전히 동일
-    printf("배열 a[1]의 값 = %d \n", *(a + 1));
+    // call by reference 함수를 사용하면 원래 변수의 값도 바뀌게 됨
+    printf("call by refernce\n");
+    swap2(&a, &b);
+    printf("결과 a : %d, b : %d\n\n", a, b);
+
     return 0;
 }
+
+//call by value
+//결과 a : 10, b : 50
+//
+//call by refernce
+//결과 a : 50, b : 10
