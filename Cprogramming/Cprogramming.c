a8255308e8c348a1d9d3ac2128e717f346ac1094
gani0325 <lge0325@bme.gachon.ac.kr>
2023-09-29 00:02:37 +0900

    ➕[Feat] 정보처리기사 기출 문제 (포인터)

--- Cprogramming/Cprogramming.c
+++ Cprogramming/Cprogramming.c
@@ -1,44 +1,18 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-// 포인터의 장점은 어느곳에 있든지 주소를 직접적으로 참조함으로써 값을 가져올 수 있고 변경할 수 있다
-
-// Call by value
-// 함수에서 값을 복사해서 전달하는 방식으로, 인자로 전달되는 변수를 함수의 매개변수에 복사한다. 
-// 원본 값을 바꿀 필요가 없는 경우
-void swap1(int a, int b) {
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-// Call by reference
-// 함수에서 값을 전달하는 대신 주소값을 전달하는 방식
-void swap2(int* a, int* b) {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
+﻿#include <stdio.h>
 
 int main() {
-    int a = 10, b = 50;
 
-    printf("call by value\n");
-    swap1(a, b);
-    printf("결과 a : %d, b : %d\n\n", a, b);
+	int ary[3];
+	int s = 0;
+	*(ary + 0) = 1;
+	ary[1] = *(ary + 0) + 2;
+	ary[2] = *ary + 3;
+	for (int i = 0; i < 3; i++) {
+		s = s + ary[i];
+	}
 
-    // call by reference 함수를 사용하면 원래 변수의 값도 바뀌게 됨
-    printf("call by refernce\n");
-    swap2(&a, &b);
-    printf("결과 a : %d, b : %d\n\n", a, b);
-
-    return 0;
+	printf("%d", s);
 }
 
-//call by value
-//결과 a : 10, b : 50
-//
-//call by refernce
-//결과 a : 50, b : 10
+// 정보처리기사 기출 문제
+// 8
