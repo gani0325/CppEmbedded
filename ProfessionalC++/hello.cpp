1123462e93028446ae810b243ddf639f3f3710b1
gani0325 <lge0325@bme.gachon.ac.kr>
2023-11-13 17:46:23 +0900

    ➕[Feat] hello world

---
+++ ProfessionalC++/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello world";
+    
+    std::string data;
+    std::cin >> data;
+    std::cout << data;
+    return 0;
+}
