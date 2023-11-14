27a4a0b371bd4f69d5883fe907f8782b33484e26
gani0325 <lge0325@bme.gachon.ac.kr>
2023-11-14 14:55:42 +0900

    🖥️[Study] 실습 10

---
+++ ProfessionalC++/examples/Chapter2/실습10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    
+    cout << "문자열 입력>>";
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++) {
+        for (int j = 0; j < i+1; j++) {
+            cout << s[j];
+        }
+        cout << endl;
+    }
+}
