f73790374c1101318e16d27afbffd4266708434a
gani0325 <lge0325@bme.gachon.ac.kr>
2023-11-24 17:21:52 +0900

    🖥️[Study] 실습 4

--- ProfessionalC++/examples/Chapter7/실습4.cpp
+++ ProfessionalC++/examples/Chapter7/실습4.cpp
@@ -2,42 +2,49 @@
 #include <string>
 using namespace std;
 
-class Book {
+class Book
+{
     string title;
     int price;
     int page;
 
 public:
-    Book(string title = "", int price = 0, int page = 0) {
-        this -> title = title;
-        this -> price = price;
-        this -> page = page;
+    Book(string title = "", int price = 0, int page = 0)
+    {
+        this->title = title;
+        this->price = price;
+        this->page = page;
     }
     string getTitie();
-    friend bool operator < (string ob2, Book ob1) ;
+    friend bool operator<(string ob2, Book ob1);
 };
 
-string Book::getTitie() {
+string Book::getTitie()
+{
     return title;
 }
 
-bool operator < (string ob2, Book ob1) {
-    if (ob2.compare(ob1.title) < 0) {
+bool operator<(string ob2, Book ob1)
+{
+    if (ob2.compare(ob1.title) < 0)
+    {
         return true;
     }
-    else {
+    else
+    {
         return false;
     }
 }
 
-
-int main() {
+int main()
+{
     Book a("청춘", 20000, 300);
     string b;
     cout << "책 이름을 입력하세요>>";
     getline(cin, b);
 
-    if (b < a) {
+    if (b < a)
+    {
         // 앞에 것이 크면 1, 뒤에 것이 크면 -1
         cout << a.getTitie() << "이 " << b << "보다 뒤에 있구나!" << endl;
     }
