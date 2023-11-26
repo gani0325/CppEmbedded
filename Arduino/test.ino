0bca7c63bfab3613c9f6ee3c79adc5a0db2e2401
gani0325 <lge0325@bme.gachon.ac.kr>
2023-11-26 23:59:06 +0900

    ➕[Feat] arduino r4 wifi 연결하기 blink

--- Arduino/test.ino
+++ Arduino/test.ino
@@ -1,32 +1,25 @@
+// // the setup function runs once when you press reset or power the board
+// void setup()
+// {
+//     // initialize digital pin LED_BUILTIN as an output.
+//     pinMode(LED_BUILTIN, OUTPUT);
+// }
 
-#include "Arduino_LED_Matrix.h"
-#include <stdint.h>
-
-ArduinoLEDMatrix matrix;
-
-const uint32_t frames[][4] =
-    {
-        {0xe0000000,
-         0x0,
-         0x0,
-         66},
-        {0x400e0000,
-         0x0,
-         0x0,
-         66}};
+// // the loop function runs over and over again forever
+// void loop()
+// {
+//     digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+//     delay(1000);                     // wait for a second
+//     digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+//     delay(1000);                     // wait for a second
+// }
 
 void setup()
 {
-    matrix.loadSequence(frames);
-    matrix.begin();
-    matrix.play(true);
-    pinMode(LED_BUILTIN, OUTPUT);
+    // put your setup code here, to run once:
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+    // put your main code here, to run repeatedly:
 }
