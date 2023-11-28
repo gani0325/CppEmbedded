2a41175be1e309727a07bf126bb3d1781cf9d9f6
gani0325 <lge0325@bme.gachon.ac.kr>
2023-11-28 16:07:37 +0900

    🖥️[Study] (RTC) AlarmCallback

--- Arduino/UNO_R4_WiFi_Real-Time_Clock/UNO_R4_WiFi_Real-Time_Clock.ino
+++ Arduino/UNO_R4_WiFi_Real-Time_Clock/UNO_R4_WiFi_Real-Time_Clock.ino
@@ -2,37 +2,41 @@
 // 시간을 설정/가져올 수 있을 뿐만 아니라 알람을 사용하여 인터럽트를 트리거
 #include "RTC.h"
 
+volatile bool irqFlag = false;
+volatile bool ledState = false;
+
+const int led = LED_BUILTIN;
+
 void setup() {
+  pinMode(led, OUTPUT);
+
   Serial.begin(9600);
 
+  // Initialize the RTC
   RTC.begin();
-  
-  RTCTime startTime(28, Month::NOVEMBER, 2023, 13, 37, 00, DayOfWeek::WEDNESDAY, SaveLight::SAVING_TIME_ACTIVE);
 
-  RTC.setTime(startTime);
+  // RTC.setTime() must be called for RTC.setPeriodicCallback to work, but it doesn't matter
+  // what date and time it's set to
+  RTCTime mytime(30, Month::JUNE, 2023, 13, 37, 00, DayOfWeek::WEDNESDAY, SaveLight::SAVING_TIME_ACTIVE);
+  RTC.setTime(mytime);
+
+  // 주기적 인터럽트를 사용하면 반복 콜백을 설정 (알람 콜백 함수, alarmtime 기간)
+  if (!RTC.setPeriodicCallback(periodicCallback, Period::ONCE_EVERY_2_SEC)) {
+    Serial.println("ERROR: periodic callback not set");
+  }
 }
 
-void loop() {
-  // 시간을 설정하고 가져와서 객체에 저장
-  RTCTime currentTime;
+void loop(){
+  if(irqFlag){
+    Serial.println("Timed CallBack");
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+    irqFlag = false;
+  }
+}
 
-  // Get current time from RTC
-  RTC.getTime(currentTime);
-
-  // Print out date (DD/MM//YYYY)
-  Serial.print(currentTime.getDayOfMonth());
-  Serial.print("/");
-  Serial.print(Month2int(currentTime.getMonth()));
-  Serial.print("/");
-  Serial.print(currentTime.getYear());
-  Serial.print(" - ");
-
-  // Print time (HH/MM/SS)
-  Serial.print(currentTime.getHour());
-  Serial.print(":");
-  Serial.print(currentTime.getMinutes());
-  Serial.print(":");
-  Serial.println(currentTime.getSeconds());
-
-  delay(1000);
+// 2초마다 표시등이 깜박
+void periodicCallback()
+{
+  irqFlag = true;
 }
