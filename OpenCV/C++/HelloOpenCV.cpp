f5aa1807cb03bae68601763b7a5efc87e2005c3b
gani0325 <lge0325@bme.gachon.ac.kr>
2023-12-28 17:00:57 +0900

    ➕[Feat] image 불러오기 by. imread

--- OpenCV/C++/HelloOpenCV.cpp
+++ OpenCV/C++/HelloOpenCV.cpp
@@ -1,5 +1,18 @@
+#include "opencv2/opencv.hpp"
 #include <iostream>
+
+using namespace cv;
+using namespace std;
 
 int main() {
 	std::cout << "Hello OpenCV" << CV_VERSION << std::endl;
+
+	Mat img = imread("lena.jpg", IMREAD_COLOR);
+
+	namedWindow("image");
+	imshow("Lena", img);
+	waitKey();
+	//destroyAllWindows();
+
+	return 0;
 }
