e750a6e6b4dafbddd706d9772ada1088f553e463
gani0325 <lge0325@bme.gachon.ac.kr>
2023-12-28 17:07:28 +0900

    ➕[Feat] video 불러오기 by. VideoCapture

--- OpenCV/C++/HelloOpenCV.cpp
+++ OpenCV/C++/HelloOpenCV.cpp
@@ -4,15 +4,30 @@
 using namespace cv;
 using namespace std;
 
-int main() {
-	std::cout << "Hello OpenCV" << CV_VERSION << std::endl;
+int main()
+{
+	//비디오 캡처 초기화
+	VideoCapture cap(0);
 
-	Mat img = imread("lena.jpg", IMREAD_COLOR);
+	//카메라가 성공적으로 열렸는지 확인을 위한 조건문
+	if (!cap.isOpened())
+	{
+		printf("Can't open the camera");
+		return -1;
+	}
 
-	namedWindow("image");
-	imshow("Lena", img);
-	waitKey();
-	//destroyAllWindows();
+	//매트릭스 초기화
+	Mat img;
+
+	//비디오 캡처 및 표시:
+	while (1)
+	{
+		cap >> img;
+		imshow("camera img", img); //창에 캡처된 프레임을 표시합니다.
+
+		if (waitKey(1) == 27)
+			break;
+	}
 
 	return 0;
 }
