58c890607c59af01b5ce98e2e2bbfa9c17d18b97
gani0325 <lge0325@bme.gachon.ac.kr>
2024-02-06 23:48:16 +0900

    ➕[Feat] 구조체 안에 enum (유효한 상수 정의)

--- ProfessionalC++/iostream.cpp
+++ ProfessionalC++/iostream.cpp
@@ -8,3 +8,18 @@ struct Student
     char name[10];
     double height;
 };
+
+struct Student
+{
+    // 상수 정의
+    enum
+    {
+        NAME_LEN = 10,
+        SHOW = 5
+    };
+
+    int number;
+    char name[NAME_LEN];
+    double height;
+    void ShowStudentInform() { ... }
+};
