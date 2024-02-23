957fe5076ca44c9f07b208775bc5349616a675aa
gani0325 <lge0325@bme.gachon.ac.kr>
2024-02-24 00:09:10 +0900

    ➕[Feat] return 문이 실행되면 복사본이 생기고 함수를 호출한 곳으로 전달된다

--- ProfessionalC++/solutions/Chapter5/5_2.cpp
+++ ProfessionalC++/solutions/Chapter5/5_2.cpp
@@ -1,10 +1,44 @@
-/*
-- 함수 호출 시 객체 전달
-2) 주소에 의한 호출 과정
-함수 호출 시 객체를 통째로 복사하여 넘기는 값에 의한 호출 대신,
-객체의 주소만 전달하는 주소의 의한 호출되도록 함
+// 객체 리턴
+#include <iostream>
+using namespace std;
 
-원본 객체를 통째로 전달하는 값에 의한 호출에 비해, 원본 객체를 복사하는 시간 소모가 없음
-매개 변수가 다눈 포인터이므로, 생성자 소멸자의 비대칭의 문제도 없다
-하지만, 매개 변수 포인터로 의도하지 않게 원본 객체를 훼손할 가능성이 있기 때문에 조심한다
+class Circle
+{
+    int radius;
+
+public:
+    Circle() { radius = 1; }
+    Circle(int radius) { this->radius = radius; }
+    void setRadius(int radius) { this->radius = radius; }
+    double getArea() { return 3.14 * radius * radius; }
+};
+
+Circle getCircle()
+{
+    Circle tmp(30);
+    // tmp 객체의 복사본 리턴
+    return tmp;
+}
+
+int main()
+{
+    // 객체가 생성됨  -> radius = 1 로 초기화된다
+    Circle c;
+    cout << c.getArea() << endl;
+
+    c = getCircle();
+    cout << c.getArea() << endl;
+}
+
+/*
+3.14
+2826
+*/
+
+/*
+- 객체 치환 및 객체 리턴
+객체 치환 : 객체의 모든 데이터가 비트 단위로 복사된다
+
+- 함수의 객체 리턴
+return 문이 실행되면 복사본이 생기고 함수를 호출한 곳으로 전달된다
 */
